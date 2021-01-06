c27d5495a6ebae6d784889938f203b86dddf2cb4
Brax-1 <ay701548@gmail.com>
2021-01-06 21:21:23 +0500

    students

--- html/studentdata.php
+++ html/studentdata.php
@@ -10,7 +10,7 @@ if(!$_SESSION['user']){
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Delete</title>
+  <title>Details</title>
   <link rel="stylesheet" href="../css/delete.css">
 </head>
 <body>
